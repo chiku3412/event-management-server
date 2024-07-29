a1a0ef799c9754a41337e0017a64d6a1e1860905
MrSatyam77 <satyamjasoliya005@gmail.com>
2024-07-29 13:16:30 +0530

    database link change

--- src/dbconnect/connect.js
+++ src/dbconnect/connect.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/event-management").then(() => {
+// mongodb://localhost:27017/event-management
+mongoose.connect("mongodb+srv://admin:123@cluster0.zljsekw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(() => {
     console.log("mongodb is connected");
 }).catch(() => {
     console.log("mongodb is not connected");
