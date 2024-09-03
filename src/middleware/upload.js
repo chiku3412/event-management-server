d9997659b3a34399531342ae76ff6266b4f24061
MrSatyam77 <satyamjasoliya005@gmail.com>
2024-09-03 12:30:30 +0530

    update upload.js

--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -13,7 +13,7 @@ const path = require('path');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        const uploadPath = path.join(__dirname, '../../../event-management-client/src/assets/images');
+        const uploadPath = path.join(__dirname, 'event-management-client/src/assets/images');
         console.log(`Resolved upload path: ${uploadPath}`);  // Log the resolved path
         cb(null, uploadPath);
     },
